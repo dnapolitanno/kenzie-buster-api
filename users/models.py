6f327b61db8c2832e6ab2a6cbfedfc7daa90f88b
dnapolitanno <dudadnapolitano@gmail.com>
2023-06-16 18:16:13 -0300

    feat: tarefa 1 finalizada

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=127, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthdate = models.DateField(null=True)
+    is_employee = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs) -> None:
+        self.is_superuser = self.is_employee
+        return super().save(*args, **kwargs)
---
+++ users/views.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView, Response, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
