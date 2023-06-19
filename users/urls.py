ce466d6031f596bde9a344dee4ad922fa4d5a8b2
dnapolitanno <dudadnapolitano@gmail.com>
2023-06-19 18:59:12 -0300

    feat: tarefa 4 finalizada

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from . import views
+from users.views import *
 
 urlpatterns = [
-    path('users/', views.UserView.as_view()),
+    path('users/', UserView.as_view()),
     path('users/login/', TokenObtainPairView.as_view()),
+    path('users/<int:user_id>/', UserDetailView.as_view()),
 ]   
